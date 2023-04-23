4e9d0db564bee2b35cf16633e2da7d86e46668a2
Pavel <pauledelweissontop@gmail.com>
2023-04-23 19:57:50 +0300

    init: start carrying out the task

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: "eslint:recommended",
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	ignorePatterns: [".eslintrc.js"],
+	rules: {
+		quotes: ["warn", "double"],
+		semi: ["warn", "always"],
+		"no-unused-vars": "warn",
+		"no-console": "warn",
+		indent: ["warn", 2],
+	},
+};
