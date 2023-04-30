d51de8a00bf768a2998250264747575322e9b826
Pavel <pauledelweissontop@gmail.com>
2023-04-30 14:01:19 +0300

    fix: remove nonexistent script in createVirtKeyboard.js

---
+++ scripts/clickShiftKey.js
@@ -0,0 +1,17 @@
+// import { clickCapslockKey } from "./typeVirtKeyboardCharacters.js";
+
+// let clickShiftKey = () => {
+//   let shiftKey = document.querySelector("#shift");
+//   let keyboard = document.querySelector(".keyboard");
+
+//   shiftKey.addEventListener("mousedown", () => {
+//     let currentLang = localStorage.getItem("virtKeyboardLang");
+//     clickCapslockKey(keyboard);
+//   });
+
+//   shiftKey.addEventListener("mouseup", () => {
+//     clickCapslockKey(keyboard);
+//   });
+// };
+
+// export default clickShiftKey;
--- scripts/createVirtKeyboard.js
+++ scripts/createVirtKeyboard.js
@@ -2,14 +2,12 @@ import createBasicStructure from "./createBasicStructure.js";
 import createKeys from "./createKeys.js";
 import defineCurrentLangByDefault from "./defineCurrentLangByDefault.js";
 import { typeVirtKeyboardCharacters } from "./typeVirtKeyboardCharacters.js";
-import clickShiftKey from "./clickShiftKey.js";
 
 let createVirtKeyboard = () => {
   createBasicStructure();
   createKeys();
   defineCurrentLangByDefault();
   typeVirtKeyboardCharacters();
-  clickShiftKey();
 };
 
 export default createVirtKeyboard;
